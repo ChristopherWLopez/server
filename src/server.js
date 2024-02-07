e7ef9c0b4516950d1c9f7fc85fc68b1cfa104145
christopher lopez <christopherwlopez89@gmail.com>
2024-02-06 18:28:49 -0800

    jest testing

--- src/server.js
+++ src/server.js
@@ -10,5 +10,20 @@ server.get('/goodbye', (_,res)=> res.send('goodbye'));
 
 server.get('/person', (_,res)=> res.send("Gandalf"));
 
+const nameValidator = (req,res,next) =>{
+    if(req.query.name){
+        req.name = req.query.name;
+        next();
+    }else{
+        next('Failed validation: No name in query');
+    }
+};
+
+server.use(nameValidator);
+
+server.get('/person', (req, res)=>{
+    res.status(200).send({ name: req.name });
+});
+
 module.exports = server;
 
